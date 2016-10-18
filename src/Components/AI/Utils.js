02f847d397c6eabdf07d1cfe02c2f24bfc4fe05c
Nick The Sick <nperez0111@gmail.com>
2016-10-18 10:38:07 -0700

    added has possible win and is able to win

--- src/Components/AI/Utils.js
+++ src/Components/AI/Utils.js
@@ -18,9 +18,6 @@ export default class AIUtils extends Logic {
             return this.twoBoardEqual(board1, this.rotateBoard(board2, 1, board))
         })
     }
-    static isWinInForOtherPlayer(player, board) {
-        return this.isWinIn(this.getOtherPlayer(player), board)
-    }
     static justMoveAnywhere(player, board) {
         /*
                 let move = []
@@ -54,6 +51,61 @@ export default class AIUtils extends Logic {
     static addToBoard(player, board) {
         const boardRankings = [4, 1, 3, 5, 7, 0, 2, 6, 8]
 
-        return ['ADD', this.returnResponse(boardRankings, cur => this.isEmptyPos(cur, board) && cur)]
+        return ['ADD', this.returnResponse(boardRankings, cur => this.isEmptyPos(cur, board) && !this.hasIllegalLineIn(player, this.add(player, board, cur)) && cur)]
+    }
+
+    static isWinInForOtherPlayer(player, board) {
+        return this.isWinIn(this.getOtherPlayer(player), board)
+    }
+    static hasPossibleLineIn(player, board) {
+        const hasBothPos = arr => this.hasPosIn(player, arr[0], board) && this.hasPosIn(player, arr[1], board)
+
+        let hasCenter = false;
+        if (this.hasCenterIn(player, board)) {
+            //if hascenter do easy computation;
+            hasCenter = this.retRes(this.pairArrangements.filter((c, i) => {
+                return i > 3;
+            }), (cur, i) => {
+                const offset = 4 + i;
+                if (hasBothPos(cur)) {
+
+                    if (this.isEmptyPos(this.pairArrangements[-2 * (offset % 2) + offset + 1][-2 * (offset % 2) + 1], board)) {
+
+                        return offset
+
+                    }
+
+                }
+
+            }, null)
+
+        }
+
+        const ret = this.retRes(this.pairArrangements, (cur, i) => {
+            if (hasBothPos(cur)) {
+                return i;
+            }
+        }, null);
+        return hasCenter === null ? false : hasCenter || ret === null ? 12 : ret;
+
+    }
+
+    static isAbleToWin(player, board) {
+
+        const pairArrOutPut = this.hasPossibleLineIn(player, board)
+
+        const hasFinalPieceToMoveIn = () => this.hasPosIn(player, this.pairCompleter[pairArrOutPut][0], board) || this.hasPosIn(player, this.pairCompleter[pairArrOutPut][1], board)
+            //this.log( pairArrOutPut );
+        if (pairArrOutPut == 12) {
+            return false
+        } else if (this.hasCenterIn(null, board) && (pairArrOutPut < 4)) {
+            return true
+        } else if (pairArrOutPut > 3 && hasFinalPieceToMoveIn() &&
+            this.isEmptyPos(this.pairArrangements[pairArrOutPut + (-2 * (pairArrOutPut % 2) + 1)][(-1 * (pairArrOutPut % 2)) + 1], board)) {
+
+            return true;
+        }
+        return false;
+
     }
 }
