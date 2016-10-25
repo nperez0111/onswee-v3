ec141838df13a35d34cd7cd60ed7cecf0aa0c822
Nick The Sick <nperez0111@gmail.com>
2016-10-25 00:39:22 -0700

    Added a test for hit of localStorage

--- src/Utils/memoizer.spec.js
+++ src/Utils/memoizer.spec.js
@@ -2,12 +2,13 @@ import memoize from './memoizer.js'
 const e = expect
 const store = {}
 const localStorageMock = {
-    getItem: a => store[a] || null,
+    getItem: a => a == 'b' ? JSON.stringify({ '["a"]': 2 }) : store[a] || null,
     setItem: (name, obj) => { store[name] = obj },
     clear: jest.fn()
 }
 global.localStorage = localStorageMock
 global.window.localStorage = localStorageMock
+
 it('calls all the right functions', () => {
     var i = 0
     const a = function() {
@@ -18,4 +19,13 @@ it('calls all the right functions', () => {
     i++
     expect(mem(0)).toBe(0)
     expect(mem(1)).toBe(1)
+})
+
+it('loads from localStorage', () => {
+    var i = 0
+    const b = function() {
+        return i
+    }
+    const mem = memoize(b)
+    expect(mem('a')).toBe(2)
 })
