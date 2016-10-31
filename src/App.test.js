ac23132e3b5281d7dbeba4a81be720960665008c
Nick The Sick <nperez0111@gmail.com>
2016-10-30 21:02:38 -0700

    To appease Travis

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+window.localStorage = {
+    getItem: a => null,
+    setItem: (a) => a
+}
+
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
 });
